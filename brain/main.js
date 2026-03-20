eaf43e642cb22eca26b44e47eecdca97970bdcde
Priyansh5812 <priyanshkrkashyap@gmail.com>
2026-03-21 00:28:10 +0530

    Website first draft completed

---
+++ brain/main.js
@@ -0,0 +1,73 @@
+// #region INIT
+
+const readMoreBtn_1 = document.querySelector('.show-more-btn-1');
+const text_content_1 = document.querySelector('.hidden-text-1');
+
+const readMoreBtn_2 = document.querySelector('.show-more-btn-2');
+const text_content_2 = document.querySelector('.hidden-text-2');
+
+const topDiv = document.querySelector('.hero');
+const navigator = document.querySelector('.navigator');
+// #endregion 
+
+// #region Readmore
+
+readMoreBtn_1.addEventListener('click' , (e)=>
+    {   
+        const span = readMoreBtn_1.querySelector('span');
+        if(text_content_1.classList.contains('active'))
+        {   
+            text_content_1.classList.remove('active');
+            text_content_1.style.maxHeight = "0px";
+            span.textContent = 'Show More';
+        }
+        else
+        {
+            
+            text_content_1.classList.add('active');
+            text_content_1.style.maxHeight = text_content_1.scrollHeight + "px";
+            span.textContent = 'Show Less';
+        }
+    });
+    
+    
+readMoreBtn_2.addEventListener('click' , (e)=>
+    {   
+        const span = readMoreBtn_2.querySelector('span');
+        if(text_content_2.classList.contains('active'))
+        {   
+            text_content_2.classList.remove('active');
+            text_content_2.style.maxHeight = "0px";
+            span.textContent = 'Show More';
+        }
+        else
+        {
+            
+            text_content_2.classList.add('active');
+            text_content_2.style.maxHeight = text_content_2.scrollHeight + "px";
+            span.textContent = 'Show Less';
+        }
+    });
+//#endregion
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+           if(navigator.classList.contains('active'))
+           {
+                navigator.classList.remove('active');
+           }
+           console.log("Visible");
+        } 
+        else {
+            if(!navigator.classList.contains('active'))
+                {
+                    navigator.classList.add('active');
+                }
+                console.log("Not Visisble");
+        }
+    });
+});
+
+observer.observe(topDiv);
+
